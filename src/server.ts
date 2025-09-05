f212a0d370ec9ce64ab5384747061ed0679c1fac
MahidunNobi <mdmahidunnobi@gmail.com>
2025-09-05 11:33:24 +0600

    Initial Setup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express from "express";
-
-const app = express();
+import { Server } from "http";
+import app from "./app";
 
 const port = 5000;
 
-app.listen(port, () => {
-  console.log("App is listening on Port", port);
-});
+const main = async () => {
+  const server: Server = app.listen(port, () => {
+    console.log("App is listening on Port", port);
+  });
+};
+main();
