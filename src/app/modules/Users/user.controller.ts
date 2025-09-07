6bc0ffe3e915ea05823bb5732b714743a8b379a3
MahidunNobi <mdmahidunnobi@gmail.com>
2025-09-07 09:13:51 +0600

    Handle Response and Error

--- src/app/modules/Users/user.controller.ts
+++ src/app/modules/Users/user.controller.ts
@@ -2,8 +2,20 @@ import { Request, Response } from "express";
 import { userServices } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-  const result = await userServices.createAdmin(req.body);
-  res.send(result);
+  try {
+    const result = await userServices.createAdmin(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin Created Successfully!",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.name,
+      error,
+    });
+  }
 };
 
 export const userController = {
