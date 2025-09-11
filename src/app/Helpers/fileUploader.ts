324aad17184d8842046d45ed2df3a284501e62b4
MahidunNobi <mdmahidunnobi@gmail.com>
2025-09-11 17:21:29 +0600

    Add-func: Upload to cloudiniary

--- src/app/Helpers/fileUploader.ts
+++ src/app/Helpers/fileUploader.ts
@@ -1,5 +1,6 @@
 import multer from "multer";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -12,6 +13,48 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+async function uploadToCloudinary() {
+  // Configuration
+  cloudinary.config({
+    cloud_name: "dlnxfhlfl",
+    api_key: "914145195697624",
+    api_secret: "ielgnGH8f1vTFoImzA1Kff1Ax8Y", // Click 'View API Keys' above to copy your API secret
+  });
+
+  // Upload an image
+  const uploadResult = await cloudinary.uploader
+    .upload(
+      "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+      {
+        public_id: "shoes",
+      }
+    )
+    .catch((error) => {
+      console.log(error);
+    });
+
+  console.log(uploadResult);
+
+  // Optimize delivery by resizing and applying auto-format and auto-quality
+  const optimizeUrl = cloudinary.url("shoes", {
+    fetch_format: "auto",
+    quality: "auto",
+  });
+
+  console.log(optimizeUrl);
+
+  // Transform the image: auto-crop to square aspect_ratio
+  const autoCropUrl = cloudinary.url("shoes", {
+    crop: "auto",
+    gravity: "auto",
+    width: 500,
+    height: 500,
+  });
+
+  console.log(autoCropUrl);
+}
+
 export const fileUploader = {
   upload,
+  uploadToCloudinary,
 };
