7877a23a3ce4d0f119d0159ae99cad7453ce2b44
MahidunNobi <mdmahidunnobi@gmail.com>
2025-09-11 17:08:45 +0600

    Add: File Uploader

--- src/app/modules/Users/user.routes.ts
+++ src/app/modules/Users/user.routes.ts
@@ -1,9 +1,17 @@
 import express, { NextFunction, Request, Response } from "express";
 import { userController } from "./user.controller";
 import auth from "../../middlewares/auth";
+import multer from "multer";
+import path from "path";
+import { fileUploader } from "../../Helpers/fileUploader";
 
 const router = express.Router();
 
-router.post("/", auth("ADMIN", "SUPER_ADMIN"), userController.createAdmin);
+router.post(
+  "/",
+  auth("ADMIN", "SUPER_ADMIN"),
+  fileUploader.upload.single("file"),
+  userController.createAdmin
+);
 
 export const UserRoutes = router;
