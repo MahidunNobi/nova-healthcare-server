acea47911c88c34ddde5795a277b895f00acad53
MahidunNobi <mdmahidunnobi@gmail.com>
2025-09-17 11:08:22 +0600

    Creating Schedule Slots

--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,5 +1,39 @@
+import { addHours, addMinutes } from "date-fns";
+import prisma from "../../../shared/prismaClient";
+
 const createSchedule = async (payload: any) => {
-  console.log(payload);
+  const { startDate, endDate, startTime, endTime } = payload;
+
+  const allSchedules = [];
+
+  const firstDate = new Date(startDate);
+  const lastDate = new Date(endDate);
+
+  const slotInterval = 30;
+
+  while (firstDate <= lastDate) {
+    const firstDateTime = addHours(firstDate, Number(startTime.split(":")[0]));
+
+    const lastDateTime = addHours(firstDate, Number(endTime.split(":")[0]));
+
+    while (firstDateTime < lastDateTime) {
+      const schedule = {
+        startDateTime: new Date(firstDateTime),
+        endDateTime: addMinutes(firstDateTime, slotInterval),
+      };
+
+      const result = await prisma.schedule.create({
+        data: schedule,
+      });
+
+      allSchedules.push(result);
+
+      firstDateTime.setTime(firstDateTime.getTime() + slotInterval * 60 * 1000);
+    }
+
+    firstDate.setDate(firstDate.getDate() + 1);
+  }
+  return allSchedules;
 };
 
 export const scheduleService = {
